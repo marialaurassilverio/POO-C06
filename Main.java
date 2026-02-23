dbff12cd7e0c93e9a88e76ab67f07cec67dc2b80
marialaurassilverio <laurinhaa.souza2609@gmail.com>
2026-02-23 20:35:31 -0300

    Exercicio 1

---
+++ Main.java
@@ -0,0 +1,17 @@
+package org.example;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int horas = 3;
+    int primeirahora = 10;
+    int segundahora = 4;
+    int terceirahora = 2;
+
+    int soma = primeirahora+segundahora+terceirahora;
+    double media = soma/horas;
+
+    System.out.println("Total: " + soma);
+    System.out.println("Total: " + media);
+        }
+    }
