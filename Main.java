bfe7fb61ab0a4a338bd649e940725cfd61b9f6d6
marialaurassilverio <laurinhaa.souza2609@gmail.com>
2026-02-23 22:28:12 -0300

    Exercício 3

--- Main.java
+++ Main.java
@@ -1,17 +1,26 @@
 package org.example;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    int horas = 3;
-    int primeirahora = 10;
-    int segundahora = 4;
-    int terceirahora = 2;
+        var valor1 = 30;
+        var valor2 = 15;
 
-    int soma = primeirahora+segundahora+terceirahora;
-    double media = soma/horas;
+        var Qtt_inteiros = 8;
+        var Qtt_Meia = 5;
 
-    System.out.println("Total: " + soma);
-    System.out.println("Total: " + media);
+        var Total_inteiros = Qtt_inteiros*valor1;
+        var Total_meia = Qtt_Meia*valor2;
+
+        System.out.println("Valor total arrecadado com ingressos inteiros: " + Total_inteiros);
+        System.out.println("Valor total arrecadado com ingressos meia: " + Total_meia);
+
+        var soma =  Total_inteiros + Total_meia;
+        System.out.println("Valor total arrecadado: " + soma);
+
+        var media = soma/(Qtt_Meia+Qtt_inteiros);
+        System.out.println("Valor medio pago por ingresso: " + media);
+
         }
     }
