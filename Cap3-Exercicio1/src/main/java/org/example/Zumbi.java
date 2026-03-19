ed2d2807401ac6988ddb9e9fa91b3fa2684e4db2
marialaurassilverio <laurinhaa.souza2609@gmail.com>
2026-03-18 21:45:09 -0300

    Exercício 1 (Zumbi) - Capítulo 3

---
+++ Cap3-Exercicio1/src/main/java/org/example/Zumbi.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Zumbi {
+    public String nome;
+    public double nivelFome;
+
+    public void atacar(){
+        System.out.println("Atacando");
+    }
+    public void resmungar(){
+        System.out.println("grrrr");
+    }
+
+}
